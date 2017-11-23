a6f3478244374ee815cf79d5b985db877dc6b811
Michael Benz <michael@benz.io>
2017-11-23 20:03:25 +1100

    better btn management

--- screenController.py
+++ screenController.py
@@ -10,19 +10,39 @@ class ScreenController(object):
             screens.AudioControl(screenCtrl=self)
         ]
         self.lcd = LCD.Adafruit_CharLCDPlate()
+        self.previousBtnState = None
     
     def lcdLoop(self):
-        buttons = ( (LCD.SELECT, self.currentScreen.btnUp),
-                    (LCD.LEFT,   self.currentScreen.btnLeft),
-                    (LCD.UP,     self.currentScreen.btnUp),
-                    (LCD.DOWN,   self.currentScreen.btnDown),
-                    (LCD.RIGHT,  self.currentScreen.btnRight)
-            )
 
-        for button in buttons:
-            if self.lcd.is_pressed(button[0]):
-                button[1]()
+        if self.btnState != self.previousBtnState:
+            if self.btnState[LCD.SELECT]:
+                self.currentScreen.btnSelect()
+            
+            elif self.btnState[LCD.LEFT]:
+                self.currentScreen.btnLeft()
+
+            elif self.btnState[LCD.UP]:
+                self.currentScreen.btnUp()
+            
+            elif self.btnState[LCD.DOWN]:
+                self.currentScreen.btnDown()
+
+            elif self.btnState[LCD.RIGHT]:
+                self.currentScreen.btnRight()
+            
+
+            self.previousBtnState = self.btnState
     
+    @property
+    def btnState(self):
+        return [
+            self.lcd.is_pressed(LCD.SELECT),
+            self.lcd.is_pressed(LCD.RIGHT),
+            self.lcd.is_pressed(LCD.DOWN),
+            self.lcd.is_pressed(LCD.UP),
+            self.lcd.is_pressed(LCD.LEFT),
+        ]
+
     def incrementScreen(self):
         newID = self.currentScreenId + 1
         self.currentScreenId = newID % len(self.screens)
